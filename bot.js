f884440e70006ae0f5c8a730a0375fd2e8342205
alone8659 <40628165+alone8659@users.noreply.github.com>
2018-06-28 12:56:56 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.NDYxNDM1NDcyMzM0MjkwOTQ0.DhYZWA.7lARPbDERCPKfND-XMYglvicTtM);
+client.login(process.env.BOT_TOKEN);
